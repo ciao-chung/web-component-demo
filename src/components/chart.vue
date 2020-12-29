167bf5c14ec6f3042b3920311af823db28f88b02
Ciao Chung <ciao0958@gmail.com>
2020-12-29 13:58:30 +0800

    chore: 加入web-component

---
+++ src/components/chart.vue
@@ -0,0 +1,20 @@
+<template>
+  <div dental-chart="root"></div>
+</template>
+
+<script lang="babel" type="text/babel">
+export default {
+  data: () => ({
+    
+  }),
+  created() {},
+}
+</script>
+
+<style lang="sass" type="text/sass" scoped>
+div[dental-chart="root"]
+  border: 5px skyblue solid
+  background-color: darken(skyblue, 30%)
+  width: 80px
+  height: 60px
+</style>
